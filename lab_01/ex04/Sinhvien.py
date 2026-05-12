e699c8c42587d19201029b997dfd1fdfafcbc577
bornbe <tienduc012003@gmail.com>
2026-05-12 11:10:59 +0700

    ex04

---
+++ lab_01/ex04/Sinhvien.py
@@ -0,0 +1,8 @@
+class SinhVien:
+    def __init__(self, _id, name, sex, major, diemTB):
+        self._id = _id
+        self._name = name
+        self._sex = sex
+        self._major = major
+        self._diemTB = diemTB
+        self._hocluc = ""
